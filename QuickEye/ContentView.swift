5f25a9c9d2f7cd267255e92ece9ed5947cd5b70a
MKFLA <mkfla.work@gmail.com>
2022-04-24 14:25:57 +0800

    Initial Commit

---
+++ QuickEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickEye
+//
+//  Created by shuo zhang on 2022/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickEye/QuickEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickEyeApp.swift
+//  QuickEye
+//
+//  Created by shuo zhang on 2022/4/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
